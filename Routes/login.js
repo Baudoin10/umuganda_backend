4d25266dfb58077cf5bb7fd9a3a0be38169b3d3f
baudoin <baudoinvicbolingo@gmail.com>
2025-01-02 13:15:02 +0200

    fixed

--- Routes/login.js
+++ Routes/login.js
@@ -103,19 +103,14 @@ const router = express.Router();
 router.post("/login", (req, res) => {
   const { username, password, role } = req.body;
 
-  // if (username && password && role) {
-  //   // Logic to authenticate user
-  //   res.status(200).send({ message: "Login successful", username, role });
-  // } else {
-  //   res.status(400).send({ message: "Invalid input" });
-  // }
-
-  if (username?.trim() && password?.trim() && role?.trim()) {
+  if (username && password && role) {
     // Logic to authenticate user
     res.status(200).send({ message: "Login successful", username, role });
   } else {
-    res.status(400).send({ message: "Invalid input: Fields cannot be empty" });
+    res.status(400).send({ message: "Invalid input" });
   }
+
+ 
 });
 
 module.exports = router;
