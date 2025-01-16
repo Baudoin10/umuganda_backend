892faffc113e61750aabdd03a19aea858e5ab8fe
baudoin <baudoinvicbolingo@gmail.com>
2025-01-16 15:30:36 +0200

    testing apies

--- Routes/events.js
+++ Routes/events.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
 
+
 // In-memory events storage
 let events = [];
 
