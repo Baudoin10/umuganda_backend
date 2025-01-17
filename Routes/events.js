5767a330979c3ca3326b39e06b5202d676843a9e
baudoin <baudoinvicbolingo@gmail.com>
2025-01-17 12:48:14 +0200

    fixing errors

--- Routes/events.js
+++ Routes/events.js
@@ -123,9 +123,7 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    
     const { title, description, address, date, day, month } = req.body;
-    
     const newEvent = {
       id: events.length + 1,
       title,
