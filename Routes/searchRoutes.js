6c3c9b73a650eae2c65db7a1cc1f2ffe185a0384
Baudoin <baudoinvicbolingo@gmail.com>
2025-06-17 11:15:13 +0200

    done

--- Routes/searchRoutes.js
+++ Routes/searchRoutes.js
@@ -1,11 +1,34 @@
 
-
 const express = require("express");
 const { searchtask } = require("../controllers/searchtaskController");
 
 const router = express.Router();
 
-// Search task route
+/**
+ * @swagger
+ * /tasks/search:
+ *   get:
+ *     summary: Search for tasks
+ *     description: Retrieve a list of tasks based on search criteria.
+ *     parameters:
+ *       - in: query
+ *         name: query
+ *         schema:
+ *           type: string
+ *         required: false
+ *         description: Search keyword for filtering tasks
+ *     responses:
+ *       200:
+ *         description: A list of matching tasks
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Task'
+ *       400:
+ *         description: Bad request
+ */
 router.get("/tasks/search", searchtask);
 
 module.exports = router;
