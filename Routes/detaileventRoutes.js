8d3cbf9b320fe351ab744c28b4194f23511bf91d
Baudoin <baudoinvicbolingo@gmail.com>
2025-06-18 09:45:25 +0200

    fixed another bug on event detail

--- Routes/detaileventRoutes.js
+++ Routes/detaileventRoutes.js
@@ -1,14 +1,3 @@
-// // detaileventRoutes.js
-
-// const express = require("express");
-// const router = express.Router();
-// const { joinEvent } = require("../controllers/detaileventController");
-
-// // Route to join an event
-// router.post("/join", joinEvent);
-
-// module.exports = router;
-
 
 const express = require("express");
 const router = express.Router();
