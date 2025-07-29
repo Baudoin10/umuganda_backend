525f36ed6c4d82f471eb990d9c22db7748468df6
Baudoin <baudoinvicbolingo@gmail.com>
2025-07-29 09:40:27 +0200

    fixed the event model

--- models/eventModel.js
+++ models/eventModel.js
@@ -1,18 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const eventSchema = new mongoose.Schema(
-//   {
-//     title: { type: String, required: true },
-//     description: { type: String, required: true },
-//     address: { type: String, required: true },
-//     date: { type: String, required: true }, 
-//     day: { type: String, required: true },
-//     month: { type: String, required: true },
-//   },
-//   { timestamps: true }
-// );
-
-// module.exports = mongoose.model("Event", eventSchema);
 
 const mongoose = require("mongoose");
 
